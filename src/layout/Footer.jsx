eb26770a41fca8b1b65e47115adce0b57f16e92d
tirmidziahmad <tirmidziahmad05@gmail.com>
2024-08-27 21:03:15 +0700

    fix:changing footer to flex

--- src/layout/Footer.jsx
+++ src/layout/Footer.jsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="bg-black text-white py-10">
-      <div className="container mx-auto items-center justify-between flex ">
+      <div className="container mx-auto items-center justify-between flex">
         {/* Logo and Copyright Section */}
         <div className="flex flex-col justify-center w-1/3">
           <img src="Logo.png" alt="Logo" className="w-52" />
@@ -9,88 +9,94 @@ function Footer() {
         </div>
 
         {/* Genre and Help Sections */}
-        <div className="grid md:grid-cols-2 w-2/3 justify-items-end">
+        <div className="flex justify-around flex-grow ">
           {/* Genre Section */}
-          <div>
-            <h3 className="text-lg font-bold mb-4 ">Genre</h3>
-            <ul className="grid grid-rows-4 grid-flow-col gap-y-3 gap-x-4 ">
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Aksi
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Anak-anak
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Anime
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Britania
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Drama
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Fantasi Ilmiah & Fantasi
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Kejahatan
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  KDrama
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Komedi
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Petualangan
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Perang
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Romantis
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Sains & Alam
-                </a>
-              </li>
-              <li>
-                <a href="#" className="hover:text-gray-400">
-                  Thriller
-                </a>
-              </li>
+          <div className="flex flex-col">
+            <h3 className="text-lg font-bold ">Genre</h3>
+            <ul className="flex gap-5">
+              <div className="flex flex-col">
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Aksi
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Anak-anak
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Anime
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Britania
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Drama
+                  </a>
+                </li>
+              </div>
+              <div className="flex flex-col">
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Fantasi Ilmiah & Fantasi
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Kejahatan
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    KDrama
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Komedi
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Petualangan
+                  </a>
+                </li>
+              </div>
+              <div className="flex flex-col">
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Perang
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Romantis
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Sains & Alam
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:text-gray-400">
+                    Thriller
+                  </a>
+                </li>
+              </div>
             </ul>
           </div>
 
           {/* Help Section */}
           <div>
-            <h3 className="text-lg font-bold mb-4 ">Bantuan</h3>
-            <ul className="grid grid-rows-4 grid-flow-col gap-y-3 gap-x-4 ">
+            <h3 className="text-lg font-bold  ">Bantuan</h3>
+            <ul className="flex-col ">
               <li>
                 <a href="#" className="hover:text-gray-400">
                   FAQ
