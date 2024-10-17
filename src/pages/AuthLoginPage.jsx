35edf8e938272bb80ae63eb97ed389b9e967fcea
Tirmidzi Ahmad <tirmidziahmad05@gmail.com>
2024-10-17 23:40:41 +0700

    fix: bg login not showing

--- src/pages/AuthLoginPage.jsx
+++ src/pages/AuthLoginPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AuthLoginTemplate from "../components/templates/AuthLoginTemplate";
 function AuthLoginPage() {
-  return <AuthLoginTemplate className="bg-[url('bgMasuk.jpeg')]" />;
+  return <AuthLoginTemplate className="bg-[url('./assets/bg/bgMasuk.jpeg')]" />;
 }
 
 export default AuthLoginPage;
